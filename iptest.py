bb1a89bc097612fb48dd7c42f6a9ce44a71e5568
Jason Martin <jhmartin@toger.us>
2018-08-20 18:29:33 -0700

    Fix persistent connections

--- iptest.py
+++ iptest.py
@@ -5,6 +5,10 @@ for x in range(0, 10000):
   r = requests.get('http://ip')
   print("SIMPLE\t" + r.text.rstrip("\r\n"))
 
+sess = requests.Session()
+adapter = requests.adapters.HTTPAdapter(pool_connections=100,pool_maxsize=100)
+sess.mount('http://',adapter)
+
 for x in range(0, 10000):
-  r = requests.get('http://ip')
+  r = sess.get('http://ip')
   print("PERSISTENT\t" + r.text.rstrip("\r\n"))
